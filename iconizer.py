f113183612f2739833e063e090e5add1cbc3e35e
Stefan Herold <stefan.herold@gmail.com>
2017-10-23 01:32:22 +0200

    Fix iconfile.py - now complete

--- iconizer.py
+++ iconizer.py
@@ -3,9 +3,13 @@
 # Simple tool to generate all necessary icon sizes for an iOS app from one 
 # pdf vector file.
 #
-# IMPORTANT: This file is work in progress and not yet complete!
+# REQUIREMENTS:
+#    $ brew install imagemagick@6
+#    $ export MAGICK_HOME=/usr/local/opt/imagemagick@6
+#    $ sudo easy_install wand
 #
 from __future__ import print_function # import Python 3 print function
+from wand.image import Image
 import json
 import os.path
 import sys
@@ -14,7 +18,6 @@ assert len(sys.argv) == 3, 'Usage: python sys.argv[0] <vector>.pdf <asset_path>'
 assert os.path.isfile(sys.argv[1]), 'Did not find file %s, expected path to a vector image file.' % sys.argv[1]
 assert os.path.isdir(sys.argv[2]), 'Did not find path to asset directory %s' % sys.argv[2]
 
-vectorImage = sys.argv[1]
 assetDir = sys.argv[2]
 contentsFile = 'Contents.json'
 contentsPath = os.path.join(assetDir, contentsFile)
@@ -25,7 +28,7 @@ with open(contentsPath, 'r') as jsonFile:
 for idx, image in enumerate(jsonData['images']):
   scale = image['scale'].split('x')[0]
   sizePT = image['size'].split('x')[0]
-  sizePX = float(sizePT) * float(scale)
+  sizePX = int(float(sizePT) * float(scale))
   imageName = "appicon_" + str(sizePX) + ".png"
 
   jsonData['images'][idx]['filename'] = imageName
@@ -37,4 +40,7 @@ for idx, image in enumerate(jsonData['images']):
     print('File %s already created... Continue' % imageName)
   else:
     print('Creating %s and update %s...' % (imageName, contentsFile), end='')
+    with Image(filename=sys.argv[1], resolution=600) as img:
+      img.resize(sizePX, sizePX)
+      img.save(filename=os.path.join(assetDir, imageName))
     print(' %s' % u'\u2705')
