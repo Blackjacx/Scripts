70546c67252457e1a0ac50d43292e79a3054a2b5
Stefan Herold <stefan.herold@gmail.com>
2019-01-22 19:17:59 +0100

    Add ability to extract issue and title from changelog

--- fastfile_imports.rb
+++
@@ -1,7 +0,0 @@
-# Splits a string into components and returns them as an array
-def split_string(str)
-  if str.nil?
-    return []
-  end
-  return str.split(",").map{ |s| s.strip || s }.reject { |s| s.empty? }
-end
---
+++ imports.rb
@@ -0,0 +1,27 @@
+# Splits a string into components and returns them as an array.
+# If the string is nil or empty or if the string just consists of delimiters it returns an empty array.
+# Params:
+# +string+:: The string to split.
+# +delimiter+:: The delimiter which should be used to split.
+def split_string(string, delimiter)
+  if string.nil?
+    return []
+  end
+  return string
+    .split(',')
+    .map{ |s| s.strip || s }
+    .reject { |s| s.empty? }
+end
+
+# Uses a section from a changelog and converts them to an array of hashes of issue number and pr title.
+# Params:
+# +section+:: The section of a changelog representing a release. The format has to be like https://raw.githubusercontent.com/Blackjacx/SHSearchBar/develop/CHANGELOG.md.
+def changelog_get_issue_numbers_and_titles(section)  
+  issues_titles = []
+  section.split(/\n/).each do |line|
+    issue = line[/\* \[#(.*?)\]\(/m, 1]
+    title = line[/\)\: (.*?) - \[@/m, 1]
+    issues_titles.push( { "issue" => issue, "title" => title } ) if issue && !issue.empty? && title && !title.empty?
+  end    
+  return issues_titles
+end
