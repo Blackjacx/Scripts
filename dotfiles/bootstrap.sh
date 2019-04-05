33361c3d7c1528ba671180eacfc7b602c50d70a6
Stefan Herold <stefan.herold@gmail.com>
2019-04-05 21:14:46 +0200

    Refactor bootstrapping scripts

--- dotfiles/bootstrap.sh
+++ dotfiles/bootstrap.sh
@@ -4,35 +4,67 @@
 # These commands are collected throughout the internet over time. 
 # It is not guaranteed that every command will work!
 #
-# I use the following instruction letters:
-# [R] ~ restart required
-#
-# DISCLAIMER: Don't execute it if you do not know what you rae doing!!!
+# DISCLAIMER: Don't execute it if you do not know what you are doing!!!
 
 #
 # Dotfiles
-###############################################################################
+########################################################################
 
 # DOTFILES="$HOME/.dotfiles"
 # git clone git@github.com:blackjacx/dotfiles.git $DOTFILES
 
 #
 # Defaults
-###############################################################################
+########################################################################
 
+#
+# Misc
+#
+
+# Set fast key repeat rate
 defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
 defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
-defaults write -g AppleShowAllExtensions -bool true # show file extensions
-defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true # stop photos from opening automatically when connecting an iphone
-defaults write -g NSAutomaticWindowAnimationsEnabled -bool false # disable animations when opening and closing windows
-defaults write com.apple.reminders RemindersDebugMenu -boolean true # Enable debug menu in Reminders to use the manual iCloud sync
-defaults write com.apple.TextEdit RichText -int 0 # Use plain text as default format in TextEdit
-defaults write com.apple.dock mru-spaces -bool false # Disable automatically rearrange Spaces based on recent use
 
+# show file extensions
+defaults write -g AppleShowAllExtensions -bool true 
+
+# stop photos from opening automatically when connecting an iphone
+defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true 
+
+# disable animations when opening and closing windows
+defaults write -g NSAutomaticWindowAnimationsEnabled -bool false 
+
+# Enable debug menu in Reminders to use the manual iCloud sync
+defaults write com.apple.reminders RemindersDebugMenu -boolean true
+
+# Use plain text as default format in TextEdit
+defaults write com.apple.TextEdit RichText -int 0 
+
+# Disable automatically rearrange Spaces based on recent use
+defaults write com.apple.dock mru-spaces -bool false
+
+# Require password as soon as screensaver or sleep mode starts
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+
+# Show filename extensions by default
+defaults write -g AppleShowAllExtensions -bool true
+
+# Enable tap-to-click
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+# Disable user interface sound effects
+defaults write com.apple.systemsound "com.apple.sound.uiaudio.enabled" -int 0
+
+#
 # Terminal
+#
 defaults write com.googlecode.iterm2 HotkeyTermAnimationDuration -float 0.000001 # makes iTerm appear quick
 
+#
 # iOS Development
+#
 defaults write http://com.apple.iphonesimulator ShowSingleTouches 1 # Shows touches in simulator - nice for recording videos
 defaults write com.apple.dt.Xcode ShowBuildOperationDuration -bool YES # Shows build time in Xcode's top activity bar
 
@@ -40,15 +72,15 @@ killall Finder # restart finder
 
 #
 # Homebrew
-###############################################################################
+########################################################################
 
-# Install Homebrew and all needed packages and casks
-./brew.sh
+# Install all neede software, Homebrew packages and casks
+./software.sh
 
 #
 # Shell (ZSH)
 # Install via Homebrew: https://stackoverflow.com/a/17649823/971329
-###############################################################################
+########################################################################
 
 export ZSH="$HOME/.oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
---
+++ dotfiles/software.sh
@@ -0,0 +1,132 @@
+#!/bin/bash
+#
+# Installs all needed software, ruby gems, Homebrew packages and casks
+#
+set -euo pipefail
+
+command -v brew >/dev/null 2>&1 || { 
+  echo "#################################################################"
+  echo "Installing Homebrew"
+  echo "#################################################################"
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+}
+
+echo "#################################################################"
+echo "Installing Software via Homebrew"
+echo "#################################################################"
+
+# Make sure we’re using the latest Homebrew
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Packages
+brew tap jzaleski/homebrew-jzaleski # for apg
+brew tap caskroom/fonts # for fonts
+
+packages=(
+  ag
+  apg # generate strong passwords
+  carthage
+  coreutils
+  curl
+  doxygen
+  e2fsprogs # e2fsprogs is a set of utilities for maintaining the ext2, ext3 and ext4 file systems.
+  ffmpeg
+  figlet
+  git # Distributed version-control system for tracking changes in source code during software development
+  grep
+  htop
+  httpie
+  hub
+  imagemagick
+  jq # Command-line JSON processor
+  mas # Mac App Store command line interface
+  mplayer
+  npm
+  node
+  openssl
+  p0f
+  pstree
+  rbenv
+  ripgrep
+  shellcheck # static analysis tool for shell scripts
+  tree
+  watch
+  wget
+  zsh
+)
+brew install ${packages[@]}
+
+
+casks=(
+  # 1password # this installs the pricy 1password 7
+  battle-net
+  brisk
+  charles
+  dash
+  deckset
+  disk-inventory-x
+  dropbox
+  emacs
+  fantastical
+  flycut
+  font-fira-code
+  font-roboto
+  font-source-code-pro
+  # firefox # to work correctly with 1password install it manually
+  gpg-suite
+  # google-chrome # to work correctly with 1password install it manually
+  handbrake
+  iterm2
+  provisionql
+  qlimagesize
+  qlmarkdown
+  qlstephen
+  quicklook-json
+  scummvm
+  sketch
+  skype
+  slack
+  sourcetree
+  spectacle
+  spotify
+  sublime-text
+  telegram
+  tunnelblick
+  typora
+  ulysses
+  vlc
+  wireshark
+  wwdc
+  zeplin
+)
+# Install brew cask
+brew tap caskroom/cask
+brew cask install ${casks[@]}
+
+# Cleanup
+brew cleanup
+brew doctor
+
+echo "#################################################################"
+echo "Installing Xcode commandline tools"
+echo "#################################################################"
+
+xcode-select --install
+
+echo "#################################################################"
+echo "Installing Mac Appstore Apps"
+echo "#################################################################"
+
+mas install 497799835  # Xcode
+
+echo "#################################################################"
+echo "Installing Ruby Gems"
+echo "#################################################################"
+
+RUBY_GEMS=(
+  bundler
+)
+sudo gem install ${RUBY_GEMS[@]}
