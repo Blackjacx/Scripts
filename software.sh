ce66a2b58ffb581ad72b9c8e3e6acfbc6d6eae90
Stefan Herold <stefan.herold@gmail.com>
2019-04-06 02:05:17 +0200

    Only switch to zsh if not done already

--- software.sh
+++ software.sh
@@ -120,12 +120,14 @@ echo "Setting up ZSH with Oh-My-Zsh"
 echo "Credits: https://stackoverflow.com/a/17649823/971329"
 echo "#################################################################"
 
-# append zsh to the end of /etc/shells
-sudo sh -c 'echo /usr/local/bin/zsh >> /etc/shells'
-# change default shell
-sudo chsh -s /usr/local/bin/zsh
-# install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+grep -q /usr/local/bin/zsh /etc/shells || {
+  # append zsh to the end of /etc/shells
+  sudo sh -c "echo /usr/local/bin/zsh >> /etc/shells"
+  # change default shell
+  sudo chsh -s /usr/local/bin/zsh
+  # install oh-my-zsh
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+}
 
 # Make first shure ZSH and oh-my-zsh are installed correctly 
 # echo "#################################################################"
