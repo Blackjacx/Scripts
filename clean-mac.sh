ff7071212a36ffde4242ce8c80df23d6424f7173
Stefan Herold <stefan.herold@gmail.com>
2019-05-10 00:31:33 +0200

    Add experimental device support clean-up code

--- clean-mac.sh
+++ clean-mac.sh
@@ -16,3 +16,28 @@ xcrun simctl delete unavailable
 
 echo "Empty Trash..."
 rm -rf ~/.Trash/*
+
+
+
+##
+## The following is highly experimental!!!
+## It is used to automatically delete iOS device support files and keeps 
+## the two most recent versions. Those are one of the biggest space
+## killers in iOS world.
+##
+
+# set -euo pipefail
+# IFS=$'\n\t'
+
+# # # files=$(ls -v Versions |awk '{print $1}' |uniq |cut -d'.' -f1 |uniq |tail -c -2)
+# # # folders=$(ls -v ${HOME}/Library/Developer/Xcode/iOS\ DeviceSupport |awk '{print $1}')
+# # folders=$(ls -vd ${HOME}/Desktop/Versions/*/ | sed 's/.$//')
+# # # folders=$(cat ~/list.txt)
+# # MAX_VERSION=$(echo $folders | awk -F/ '{print $NF}')
+# # # printf '%s\n' ${folders[@]}
+# # # printf '%s\n' ${#folders[@]}
+# # printf '%s\n' $MAX_VERSION
+
+# folders=$(ls -vd ${HOME}/Library/Developer/Xcode/iOS\ DeviceSupport/*/ | sed 's/.$//')
+# keep=$(echo "${folders[*]}" | sort -t. -nr | awk -F. 'a[$1]++<1' | cut -d' ' -f1 | head -2 | paste -s -d '|' -)
+# echo "${folders[*]}" | grep -vE $keep
