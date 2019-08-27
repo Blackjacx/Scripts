b9981061d8b037d42df527845b3caafcc9d38969
Stefan Herold <stefan.herold@gmail.com>
2019-08-27 14:57:07 +0200

    Add info for production/sandbox to script & modularization

--- push-apns.sh
+++ push-apns.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 set -euo pipefail
 IFS=$'\n\t'
 
@@ -6,22 +7,30 @@ function usage() {
   cat <<EOM
   $1
   Usage:
-    $0 <message> <auth_token> <device_token>
+    $0 <message> <auth_token> <device_token> <bundle_id>
 EOM
   exit 0
 }
 
+# endpoint=https://api.push.apple.com:443
+endpoint=https://api.sandbox.push.apple.com:443
+urlPath=/3/device/
+
 message=${1:-}
 auth_token=${2:-}
 device_token=${3:-}
+bundleId=${4:-}
 
 [[ -z "$message" ]] && usage "No message given!"
 [[ -z "$auth_token" ]] && usage "No auth token given!"
 [[ -z "$device_token" ]] && usage "No device token given!"
+[[ -z "$bundleId" ]] && usage "No bundle id given!"
+
+url=$endpoint$urlPath$device_token
 
 curl -v \
-     -d '{"aps":{"alert":"'"$message"'"}}' \
-     -H "apns-topic: com.ioki.passenger" \
-     -H "Authorization: Bearer $auth_token" \
      --http2 \
-     "https://api.push.apple.com/3/device/$device_token"
+     --header "apns-topic: ${bundleId}" \
+     --header "Authorization: bearer ${auth_token}" \
+     -data '{"aps":{"alert":"'"$message"'"}}' \
+     "${url}"
