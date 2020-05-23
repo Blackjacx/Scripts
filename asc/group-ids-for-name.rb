12f5c513cb893c381361b65259d084c22d88492f
Stefan Herold <stefan.herold@gmail.com>
2020-05-24 01:57:10 +0200

    Implement id retrieval in ruby

---
+++ asc/group-ids-for-name.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/ruby
+
+require 'net/http'
+require 'faraday'
+require 'faraday_middleware'
+
+def usage(message:)
+  puts message
+  puts "Usage: export ASC_AUTH_HEADER=\"$(asc_auth_header)\" && #{$0} <tester_group_name>"
+  puts "Quit..."
+end
+
+group = ARGV[0]
+
+if group.nil?
+  usage(message: "Tester group name missing!")
+  exit(1) 
+end
+
+conn = Faraday.new 'https://api.appstoreconnect.apple.com/v1' do |c|
+  c.response :json, :content_type => /\bjson$/
+end
+
+conn.headers = {'Content-Type' => 'application/json', 'Authorization' => ENV["ASC_AUTH_HEADER"]}
+
+conn.get('betaGroups') do |req|
+  # req.params['limit'] = 100
+  # req.body = {query: 'salmon'}.to_json
+end
+
+puts response.body['data']
+  .select { |array| array['attributes']['name'] == "#{group}" }
+  .map { |group| group['id'] }
+  .join(' ')
