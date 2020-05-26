e9ff5d37b9241ee172c33b771645f127b213a0a4
Stefan Herold <stefan.herold@gmail.com>
2020-05-26 14:12:35 +0200

    Add debug info and ability to get all groupids by omitting group name

--- asc/group-ids-for-name.sh
+++ asc/group-ids-for-name.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -eo pipefail
 
 url="https://api.appstoreconnect.apple.com/v1"
 json_content_type="Content-Type: application/json"
@@ -13,12 +13,35 @@ usage() {
   echo "Quit..."
 }
 
-if [ -z "$group" ]; then usage "Tester group name missing!"; exit 1; fi
 if [ -z "$ASC_AUTH_HEADER" ]; then usage "Authorization header environment variable missing!"; exit 1; fi
 
 # Get all group ids
-ids=$(curl -g -s "$url/betaGroups" -H  "$json_content_type" -H "Authorization: $ASC_AUTH_HEADER" \
-  | jq ".data[] | select(.attributes.name == \"$group\") | .id" \
-  | awk -F'"' '{print $2}')
+groups_json=$(curl -g -s "$url/betaGroups" -H  "$json_content_type" -H "Authorization: $ASC_AUTH_HEADER")
+
+if [ -z "$group" ]; then 
+  ids=$(echo $groups_json | jq ".data[].id" | awk -F'"' '{print $2}')
+else
+  ids=$(echo $groups_json | jq ".data[] | select(.attributes.name == \"$group\") | .id" | awk -F'"' '{print $2}')
+fi
 
 echo $ids
+
+# all_names=$(echo $groups_json | jq '.data[] | ( .id, .attributes )')
+# all_ids=$(echo $groups_json | jq ".data[].id" | awk -F'"' '{print $2}')
+
+# printf '%s\n' "${ids[@]}"
+# printf '%s\n' "${all_ids[@]}"
+# printf '%s\n' "${groups_json[@]}"
+# printf '%s\n' "${all_names[@]}"
+
+# app_links=($(echo $groups_json | jq ".data[] | select(.attributes.name == \"$group\") | .relationships.app.links.self" | awk -F'"' '{print $2}'))
+# apps_json=$(curl -g -s "$url/apps" -H  "$json_content_type" -H "Authorization: $ASC_AUTH_HEADER")
+
+# for link in "${app_links[@]}"; do
+#   echo "Getting related app for beta group $link..."
+#   app_id=$(curl -g -s "$link" -H  "$json_content_type" -H "Authorization: $ASC_AUTH_HEADER" \
+#     | jq '.data.id' | awk -F'"' '{print $2}')
+
+#   curl -g -s "$url/apps/$app_id" -H  "$json_content_type" -H "Authorization: $ASC_AUTH_HEADER" \
+#     | jq '.data | ( .id, .attributes )'
+# done
