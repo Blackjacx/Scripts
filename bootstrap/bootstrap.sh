9aa674270be73a531a4cfb109ce01f4a729c05a2
Stefan Herold <stefan.herold@gmail.com>
2021-03-22 13:18:28 +0100

    Add app switcher on all screens

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -98,6 +98,8 @@ configureSystem() {
   defaults write com.apple.dock autohide-delay -float 0
   # Move the dock to the left
   defaults write com.apple.Dock orientation -string right
+  # show app switcher on all displays - useful when you have a lot displays
+  defaults write com.apple.Dock appswitcher-all-displays -bool true && killall Dock
 
   #
   # Terminal
