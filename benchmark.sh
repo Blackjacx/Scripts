bf4f64dd2725a8f25ac655000196990e37e9bc65
Stefan Herold <794372+Blackjacx@users.noreply.github.com>
2023-12-12 13:52:05 +0100

    [script] Resolve shellcheck warnings benchmark.sh

--- benchmark.sh
+++ benchmark.sh
@@ -2,12 +2,12 @@
 set -euo pipefail
 
 # Mac Perofrmance Benchmark
-# 
+#
 # On my fastest Mac it takes 0:26:42 min
 
 osascript -e 'tell app "Xcode" to quit'
-rm -rf ${HOME}/Library/Developer/Xcode/DerivedData
+rm -rf "${HOME}/Library/Developer/Xcode/DerivedData"
 tmp_dir=$(mktemp -d)
-cd $tmp_dir
-git clone git@github.com:Blackjacx/Columbus.git $tmp_dir/Columbus/
+cd "$tmp_dir"
+git clone git@github.com:Blackjacx/Columbus.git "$tmp_dir/Columbus/"
 time xcodebuild -showBuildTimingSummary -destination "platform=iOS Simulator,name=iPhone 7" -workspace "$tmp_dir/Columbus/Columbus.xcworkspace" -scheme "Columbus-iOS" build > /dev/null
