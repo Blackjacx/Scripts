d894306aafbdc95395ddb20d829dc31d01623964
Stefan Herold <794372+Blackjacx@users.noreply.github.com>
2023-12-12 13:53:23 +0100

    [nvim] Configure git-blame

--- bootstrap/config_dirs/nvim/lua/user/plugins/git-blame.lua
+++ bootstrap/config_dirs/nvim/lua/user/plugins/git-blame.lua
@@ -1,4 +1,18 @@
 return {
 	"f-person/git-blame.nvim",
 	event = "VeryLazy",
+	config = function()
+		-- import comment plugin safely
+		local plugin = require("gitblame")
+
+		-- Note how the `gitblame_` prefix is omitted in `setup`
+		plugin.setup({
+			enabled = true,
+			message_template = "  <summary> • <date> • <author> • <sha>",
+			date_format = "%r",
+			message_when_not_committed = "  Oh please, commit this !",
+			-- delay = 1000,
+			use_blame_commit_file_urls = true,
+		})
+	end,
 }
