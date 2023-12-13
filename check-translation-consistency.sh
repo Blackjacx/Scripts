75060213f1e125d1a2f571625f82edc6ad0ddca5
Stefan Herold <794372+Blackjacx@users.noreply.github.com>
2023-12-13 22:36:39 +0100

    Apply re-indentations

--- check-translation-consistency.sh
+++ check-translation-consistency.sh
@@ -14,16 +14,16 @@ KEYS=()
 
 # Iterate over all localization files
 for ITEM in "${ITEMS[@]}"; do
-  LINES=( $(grep '^\".*\"' "$ITEM") )
+    LINES=( $(grep '^\".*\"' "$ITEM") )
 
-  # LINES=( $(cat $ITEM | grep '^\".*\"') )
-  echo "$red$ITEM $blue(${#LINES[@]} Keys)$white"
+    # LINES=( $(cat $ITEM | grep '^\".*\"') )
+    echo "$red$ITEM $blue(${#LINES[@]} Keys)$white"
 
-  # iterate over lines of the translation file and collect all keys
-  for LINE in "${LINES[@]}"; do
-    KEY=$(echo $LINE|cut -d'"' -f2|cut -d'"' -f1)
-    KEYS+=("$KEY")
-  done
+    # iterate over lines of the translation file and collect all keys
+    for LINE in "${LINES[@]}"; do
+        KEY=$(echo "$LINE" | cut -d'"' -f2 | cut -d'"' -f1)
+        KEYS+=("$KEY")
+    done
 done
 
 # sort and uniqueing all keys
@@ -32,11 +32,11 @@ echo -e "Found $red${#KEYS[@]}$white keys in $red${#ITEMS[@]}$white files!\n"
 
 # Iterate over all keys
 for KEY in "${KEYS[@]}"; do
-  # Find usages of localization keys - ignore paths from .strings files
-  CMD_FIND="find $(pwd) -name \"*.swift\" -type f ! -path \"*.strings*\" -exec grep -n --quiet \"$KEY\" {} +"
-  if ! eval "$CMD_FIND"; then
-    UNUSED+=("$KEY")
-  fi 
+    # Find usages of localization keys - ignore paths from .strings files
+    CMD_FIND="find $(pwd) -name \"*.swift\" -type f ! -path \"*.strings*\" -exec grep -n --quiet \"$KEY\" {} +"
+    if ! eval "$CMD_FIND"; then
+        UNUSED+=("$KEY")
+    fi
 done
 
 # Status Reporting
--- raycast/copy-last-screenshot.swift
+++ raycast/copy-last-screenshot.swift
@@ -18,8 +18,8 @@ import Cocoa
 
 let query = NSMetadataQuery()
 guard let lastScreenshot = query.searchScreenshots()?.first, let path = lastScreenshot.value(forAttribute: "kMDItemPath") as? String else {
-  print("Cannot find screenshot")
-  exit(1)
+    print("Cannot find screenshot")
+    exit(1)
 }
 
 let fileURL = URL(fileURLWithPath: path)
@@ -31,21 +31,20 @@ print("Copied last screenshot")
 // MARK: - Convenience
 
 extension NSMetadataQuery {
-  func searchScreenshots() -> [NSMetadataItem]? {
-    predicate = NSPredicate(format: "kMDItemIsScreenCapture = 1")
-    sortDescriptors = [NSSortDescriptor(key: "kMDItemFSCreationDate", ascending: false)]
+    func searchScreenshots() -> [NSMetadataItem]? {
+        predicate = NSPredicate(format: "kMDItemIsScreenCapture = 1")
+        sortDescriptors = [NSSortDescriptor(key: "kMDItemFSCreationDate", ascending: false)]
 
-    NotificationCenter.default.addObserver(forName: .NSMetadataQueryDidFinishGathering, object: nil, queue: nil) { [weak self] _ in
-      self?.disableUpdates()
-      self?.stop()
-      CFRunLoopStop(CFRunLoopGetCurrent());
+        NotificationCenter.default.addObserver(forName: .NSMetadataQueryDidFinishGathering, object: nil, queue: nil) { [weak self] _ in
+            self?.disableUpdates()
+            self?.stop()
+            CFRunLoopStop(CFRunLoopGetCurrent())
+        }
+
+        guard start() else { return nil }
+
+        CFRunLoopRun()
+
+        return results.compactMap { $0 as? NSMetadataItem }
     }
-
-    guard start() else { return nil }
-
-    CFRunLoopRun()
-
-    return results.compactMap { $0 as? NSMetadataItem }
-  }
 }
-
