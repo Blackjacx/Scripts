b698b5f169a67563387252563a7d3ce7eed91bab
Stefan Herold <794372+Blackjacx@users.noreply.github.com>
2023-12-13 22:37:19 +0100

    [nvim] Disable line wrapping

--- bootstrap/config_dirs/nvim/lua/user/core/options.lua
+++ bootstrap/config_dirs/nvim/lua/user/core/options.lua
@@ -11,7 +11,7 @@ opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 
 -- line wrapping
-opt.wrap = true -- disable line wrapping
+opt.wrap = false -- disable line wrapping
 
 -- search settings
 opt.ignorecase = true -- ignore case when searching
