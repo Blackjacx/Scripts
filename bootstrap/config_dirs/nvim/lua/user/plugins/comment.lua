b3909f542bf57f9ed28dc1b46fcaf928819af104
Stefan Herold <794372+Blackjacx@users.noreply.github.com>
2023-12-13 22:35:28 +0100

    [nvim] Generalize comment plugin config

--- bootstrap/config_dirs/nvim/lua/user/plugins/comment.lua
+++ bootstrap/config_dirs/nvim/lua/user/plugins/comment.lua
@@ -5,13 +5,13 @@ return {
 		"JoosepAlviste/nvim-ts-context-commentstring",
 	},
 	config = function()
-		-- import comment plugin safely
-		local comment = require("Comment")
+		-- import plugin safely
+		local plugin = require("Comment")
 
 		local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
 
-		-- enable comment
-		comment.setup({
+		-- enable plugin
+		plugin.setup({
 			-- for commenting tsx and jsx files
 			pre_hook = ts_context_commentstring.create_pre_hook(),
 		})
