64f0e0535a45bf103ad09beaa3f34596530bc777
Stefan Herold <794372+Blackjacx@users.noreply.github.com>
2023-12-13 22:34:22 +0100

    [nvim] Add notification plugins

---
+++ bootstrap/config_dirs/nvim/lua/user/plugins/nvim-notify.lua
@@ -0,0 +1,11 @@
+return {
+	"rcarriga/nvim-notify",
+	event = { "VeryLazy" },
+	config = function()
+		-- import plugin safely
+		local plugin = require("notify")
+
+		-- enable
+		plugin.setup({})
+	end,
+}
