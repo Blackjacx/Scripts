76dbf623d52666b625a0f9db1cac8cafde4bc5d1
Stefan Herold <794372+Blackjacx@users.noreply.github.com>
2024-02-22 13:45:45 +0100

    Make `tws` show day instead of week

--- bootstrap/zsh_config_files/aliases.zsh
+++ bootstrap/zsh_config_files/aliases.zsh
@@ -140,12 +140,11 @@ alias ddd='osascript -e "tell application \"Finder\" to move POSIX file \"${HOME
 
 alias twby="timew balance 2023-11-13 - today" # balance from the beginning of all records (not including current day)
 alias twb="timew balance 2023-11-13 - tomorrow" # balance from the beginning of all records (including today)
-alias tws="timew summary :week :ids"
+alias tws="timew summary :day :ids"
 alias twsa="timew summary :all :ids"
 alias twd="timew day summary :ids rc.reports.day.hours=auto"
 alias tww="timew week summary :ids rc.reports.week.hours=auto"
 alias twm="timew month summary :ids rc.reports.month.hours=auto"
-
 
 #-------------------------------------------------------------------------------
 # File System
