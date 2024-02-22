972fc76bedf799d2f6055b8164d9a6c0961b5d4a
Stefan Herold <794372+Blackjacx@users.noreply.github.com>
2024-02-22 13:34:58 +0100

    [NVIM] Add docs for some plugins
    - Treesitter
    - Mason
    - Leap

--- bootstrap/config_dirs/nvim/lua/user/plugins/lsp/mason.lua
+++ bootstrap/config_dirs/nvim/lua/user/plugins/lsp/mason.lua
@@ -1,3 +1,6 @@
+-- Portable package manager for Neovim that runs everywhere Neovim runs.
+-- Easily install and manage LSP servers, DAP servers, linters, and formatters.
+
 return {
 	"williamboman/mason.nvim",
 	dependencies = {
