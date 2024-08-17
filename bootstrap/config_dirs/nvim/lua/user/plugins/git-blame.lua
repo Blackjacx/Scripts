5cb696abb1d0f1032b99bf593fac56c21d533731
Stefan Herold <794372+Blackjacx@users.noreply.github.com>
2024-08-18 00:04:28 +0200

    Remove message for uncommitted lines

--- bootstrap/config_dirs/nvim/lua/user/plugins/git-blame.lua
+++ bootstrap/config_dirs/nvim/lua/user/plugins/git-blame.lua
@@ -10,7 +10,7 @@ return {
 			enabled = true,
 			message_template = "  <summary> • <date> • <author> • <sha>",
 			date_format = "%r",
-			message_when_not_committed = "  Oh please, commit this !",
+			message_when_not_committed = "",
 			-- delay = 1000,
 			use_blame_commit_file_urls = false,
 		})
