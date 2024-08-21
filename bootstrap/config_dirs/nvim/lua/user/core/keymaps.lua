df2d0911aa28035c0e5f71a711bf5d6eeda101bd
Stefan Herold <794372+Blackjacx@users.noreply.github.com>
2024-08-21 23:04:34 +0200

    Centralize key maps in one file

--- bootstrap/config_dirs/nvim/lua/user/core/keymaps.lua
+++ bootstrap/config_dirs/nvim/lua/user/core/keymaps.lua
@@ -16,11 +16,11 @@ keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 -- delete single character without copying into register
 keymap.set("n", "x", '"_x', { desc = "Delete single character without copying into register" })
 
--- increment/decrement numbers
+-- Increment/decrement numbers
 keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" })
 keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
 
--- window management
+-- Window management
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "" }) -- split window vertically
 keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width & height
@@ -42,18 +42,34 @@ keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>") -- toggle split window max
 -- nvim-tree
 keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>") -- toggle file explorer
 
--- telescope
-keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
-keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
-keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
-keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
-keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
+-- Telescope
+keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", { desc = "Fuzzy find files in cwd" }) -- find files within current working directory, respects .gitignore
+keymap.set("n", "<leader>fs", ":Telescope live_grep<CR>", { desc = "Find string in cwd" })
+keymap.set("n", "<leader>fc", ":Telescope grep_string<CR>", { desc = "Find string under cursor in cwd" }) -- find string under cursor in current working directory
+keymap.set("n", "<leader>fr", ":Telescope oldfiles<CR>", { desc = "Fuzzy find recent files" })
+keymap.set("n", "<leader>fb", ":Telescope buffers<CR>", { desc = "list open buffers in current neovim instance" })
+keymap.set("n", "<leader>fh", ":Telescope help_tags<CR>", { desc = "List Help Tags" })
+keymap.set("n", "<leader>ft", ":TodoTelescope<CR>", { desc = "Find todos" }) -- find todos in cwd
 
--- telescope git commands (not on youtube nvim video)
-keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<cr>") -- list all git commits (use <cr> to checkout) ["gc" for git commits]
-keymap.set("n", "<leader>gfc", "<cmd>Telescope git_bcommits<cr>") -- list git commits for current file/buffer (use <cr> to checkout) ["gfc" for git file commits]
-keymap.set("n", "<leader>gb", "<cmd>Telescope git_branches<cr>") -- list git branches (use <cr> to checkout) ["gb" for git branch]
-keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<cr>") -- list current changes per file with diff preview ["gs" for git status]
+-- Telescope Git
+keymap.set("n", "<leader>gC", ":Telescope git_commits<CR>", { desc = "All Commits" }) -- use <cr> to checkout ["gc" for git commits]
+keymap.set("n", "<leader>gh", ":Telescope git_bcommits<CR>", { desc = "File History" }) -- list git commits for current file/buffer (use <cr> to checkout) ["gfc" for git file commits]
+keymap.set("n", "<leader>gb", ":Telescope git_branches<CR>", { desc = "Git Branches" }) -- list git branches (use <cr> to checkout) ["gb" for git branch]
+keymap.set("n", "<leader>gs", ":Telescope git_status<CR>", { desc = "Git Status" })
+
+-- Neogit
+keymap.set("n", "<leader>gg", ":Neogit<CR>", { desc = "Neogit" })
+keymap.set("n", "<leader>gc", ":Neogit commit<CR>", { desc = "Neogit commit" })
+keymap.set("n", "<leader>gl", ":Neogit pull<CR>", { desc = "Neogit pull" })
+keymap.set("n", "<leader>gp", ":Neogit push<CR>", { desc = "Neogit push" })
+-- keymap.set("n", "<leader>gB", ":Git blame<CR>", { desc = "Git Blame" })
+
+-- GitSigns
+-- keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", { desc = "Preview current hunk" })
+-- keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", { desc = "Git Toggle Line Blame" })
+keymap.set("n", "<leader>gB", ":Gitsigns blame<CR>", { desc = "Git Blame" })
+
+-- Vim Fugitive (Git)
 
 -- restart lsp server
 keymap.set("n", "<leader>rs", ":LspRestart<CR>", { desc = "Restart lsp server" })
--- bootstrap/config_dirs/nvim/lua/user/plugins/telescope.lua
+++ bootstrap/config_dirs/nvim/lua/user/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
 	"nvim-telescope/telescope.nvim",
-	branch = "0.1.x",
+	-- branch = "0.1.x",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
@@ -25,14 +25,5 @@ return {
 		})
 
 		telescope.load_extension("fzf")
-
-		-- set keymaps
-		local keymap = vim.keymap -- for conciseness
-
-		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-		keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-		keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
-		keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
 	end,
 }
