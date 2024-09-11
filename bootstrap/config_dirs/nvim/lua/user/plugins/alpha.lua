f7b0234a44012fdeeadcc284c33ef352874a6207
Stefan Herold <794372+Blackjacx@users.noreply.github.com>
2024-09-11 11:29:11 +0200

    Add Alpha plugin to Neovim

---
+++ bootstrap/config_dirs/nvim/lua/user/plugins/alpha.lua
@@ -0,0 +1,41 @@
+return {
+	"goolord/alpha-nvim",
+	event = "VimEnter",
+	-- config = function()
+	-- 	-- require("alpha").setup(require("alpha.themes.dashboard").config)
+	-- 	require("alpha").setup(require("alpha.themes.theta").config)
+	-- 	-- require("alpha").setup(require("alpha.themes.startify").config)
+	-- end,
+	config = function()
+		local alpha = require("alpha")
+		local dashboard = require("alpha.themes.dashboard")
+
+		-- Set header
+		dashboard.section.header.val = {
+			"                                                     ",
+			"  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+			"  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+			"  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+			"  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+			"  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+			"  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+			"                                                     ",
+		}
+
+		-- Set menu
+		dashboard.section.buttons.val = {
+			dashboard.button("n", "  > New File", "<cmd>ene<CR>"),
+			dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
+			dashboard.button("SPC fr", "󰱼  > Find Recent File", "<cmd>Telescope oldfiles<CR>"),
+			dashboard.button("SPC ff", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
+			dashboard.button("SPC wr", "󰁯  > Restore Session", "<cmd>SessionRestore<CR>"),
+			dashboard.button("q", "  > Quit NVIM", "<cmd>qa<CR>"),
+		}
+
+		-- Send config to alpha
+		alpha.setup(dashboard.opts)
+
+		-- Disable folding on alpha buffer
+		vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
+	end,
+}
