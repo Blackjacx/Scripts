59fe0668ea6373a6fd1a2ab63fd56c95c69fb324
Stefan Herold <794372+Blackjacx@users.noreply.github.com>
2024-09-11 11:33:38 +0200

    Extending window-specific key maps

--- bootstrap/config_dirs/nvim/lua/user/core/keymaps.lua
+++ bootstrap/config_dirs/nvim/lua/user/core/keymaps.lua
@@ -21,16 +21,27 @@ keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" })
 keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
 
 -- Window management
-keymap.set("n", "<leader>sv", "<C-w>v", { desc = "" }) -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width & height
-keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
+keymap.set("n", "<leader>bd", ":bdelete<CR>", { desc = "Delete buffer" }) -- fails if buffer modified
+keymap.set("n", "<leader>bw", ":bwipeout<CR>", { desc = "Wipeout buffer" }) -- discards buffer modifications
+keymap.set("n", "<leader>bn", ":bnext<CR>", { desc = "Next buffer" })
+keymap.set("n", "<leader>bp", ":bprevious<CR>", { desc = "Previous buffer" })
 
-keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
-keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
-keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
-keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split vertically" })
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split horizontally" })
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make split windows equal width" })
+keymap.set("n", "<leader>sx", ":close<CR>", { desc = "Close current split window" })
+
+keymap.set("n", "<leader>to", ":tabnew<CR>", { desc = "Open new tab" })
+keymap.set("n", "<leader>tx", ":tabclose<CR>", { desc = "Close tab" })
+keymap.set("n", "<leader>tn", ":tabn<CR>", { desc = "Next tab" })
+keymap.set("n", "<leader>tp", ":tabp<CR>", { desc = "Previous tab" })
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
+
+-- Search and replace all occurrences of word under cursor in file (https://neovim.discourse.group/t/non-buggy-keymap-to-search-and-replace-word-under-cursor/4314/4)
+-- PROBLEM: This replaces also parts of words which contain the word under
+-- cursor.
+-- A better approach is https://www.reddit.com/r/neovim/comments/18a7d8i/how_to_quickly_select_and_change_all_occurrences/
+-- keymap.set("n", "<Leader>c", [[:%s/<C-r><C-w>//g<Left><Left>]], { desc = "Search and replace" })
 
 ----------------------
 -- Plugin Keybinds
@@ -58,6 +69,8 @@ keymap.set("n", "<leader>gp", ":Neogit push<CR>", { desc = "Neogit push" })
 keymap.set("n", "<leader>gB", ":Gitsigns blame<CR>", { desc = "Git Blame" })
 
 -- Vim Fugitive (Git)
+
+-- Misc
 
 -- restart lsp server
 keymap.set("n", "<leader>rs", ":LspRestart<CR>", { desc = "Restart lsp server" })
