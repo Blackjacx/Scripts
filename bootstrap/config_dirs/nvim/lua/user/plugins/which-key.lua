57189a804bcbbcb91de3ce0700e9c9f6cdbd5b5a
Stefan Herold <794372+Blackjacx@users.noreply.github.com>
2024-09-11 11:31:08 +0200

    Add special key maps for buffers using which-key

--- bootstrap/config_dirs/nvim/lua/user/plugins/which-key.lua
+++ bootstrap/config_dirs/nvim/lua/user/plugins/which-key.lua
@@ -1,6 +1,5 @@
 return {
 	"folke/which-key.nvim",
-	lazy = true,
 	event = "VeryLazy",
 	init = function()
 		vim.o.timeout = true
@@ -10,6 +9,36 @@ return {
 		-- your configuration comes here
 		-- or leave it empty to use the default settings
 		-- refer to the configuration section below
+		spec = {
+			-- { "<leader>f", group = "file" }, -- group
+			-- { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find File", mode = "n" },
+			-- { "<leader>fb", function() print("hello") end, desc = "Foobar" },
+			-- { "<leader>fn", desc = "New File" },
+			-- { "<leader>f1", hidden = true }, -- hide this keymap
+			-- { "<leader>w", proxy = "<c-w>", group = "windows" }, -- proxy to window mappings
+			{
+				"<leader>b",
+				group = "buffers",
+				expand = function()
+					return require("which-key.extras").expand.buf()
+				end,
+			},
+			-- {
+			--   -- Nested mappings are allowed and can be added in any order
+			--   -- Most attributes can be inherited or overridden on any level
+			--   -- There's no limit to the depth of nesting
+			--   mode = { "n", "v" }, -- NORMAL and VISUAL mode
+			--   { "<leader>q", "<cmd>q<cr>", desc = "Quit" }, -- no need to specify mode since it's inherited
+			--   { "<leader>w", "<cmd>w<cr>", desc = "Write" },
+			-- }
+			{
+
+				"<leader>?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer Local Keymaps (which-key)",
+			},
+		},
 	},
 }
-
