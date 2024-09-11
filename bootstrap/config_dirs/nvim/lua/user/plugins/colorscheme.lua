d85553a8dd0086d7df3f6398054f56375b84e004
Stefan Herold <794372+Blackjacx@users.noreply.github.com>
2024-09-11 11:37:49 +0200

    Add tokyonight color scheme

--- bootstrap/config_dirs/nvim/lua/user/plugins/colorscheme.lua
+++ bootstrap/config_dirs/nvim/lua/user/plugins/colorscheme.lua
@@ -1,10 +1,11 @@
 return {
 	-- "ellisonleao/gruvbox.nvim",
 	"sainnhe/gruvbox-material",
+	-- "folke/tokyonight.nvim",
 	priority = 1000, -- make sure to load this before all the other start plugins
 	config = function()
 		-- load the colorscheme here
 		vim.cmd([[colorscheme gruvbox-material]])
+		-- vim.cmd([[colorscheme tokyonight]])
 	end,
 }
-
