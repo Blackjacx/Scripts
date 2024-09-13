921f970c42654539a02f2407079763c25f8a9cba
Stefan Herold <794372+Blackjacx@users.noreply.github.com>
2024-09-13 10:46:47 +0200

    Add keymap to open file/line in GitHub web
    🔥🔥🔥

--- bootstrap/config_dirs/nvim/lua/user/plugins/vim-fugitive.lua
+++ bootstrap/config_dirs/nvim/lua/user/plugins/vim-fugitive.lua
@@ -1,4 +1,11 @@
 return {
 	"tpope/vim-fugitive",
-	-- event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		"tpope/vim-rhubarb",
+	},
+	keys = {
+		{ "<leader>gy", "<CMD>GBrowse<CR>", desc = "Open current file in web" },
+		{ "<leader>gY", "<CMD>.GBrowse<CR>", desc = "Open current line in web" },
+	},
+	event = { "BufReadPre", "BufNewFile" },
 }
