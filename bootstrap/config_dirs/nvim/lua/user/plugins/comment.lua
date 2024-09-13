9758f84b3b599d122d396de08ea80932886e61e8
Stefan Herold <794372+Blackjacx@users.noreply.github.com>
2024-09-13 10:48:45 +0200

    Add documentation for plugins

--- bootstrap/config_dirs/nvim/lua/user/plugins/comment.lua
+++ bootstrap/config_dirs/nvim/lua/user/plugins/comment.lua
@@ -1,3 +1,6 @@
+-- Smart and powerful comment plugin for neovim. Supports treesitter,
+-- dot repeat, left-right/up-down motions, hooks, and more.
+-- https://github.com/numToStr/Comment.nvim
 return {
 	"numToStr/Comment.nvim",
 	event = { "BufReadPre", "BufNewFile" },
--- bootstrap/config_dirs/nvim/lua/user/plugins/leap.lua
+++ bootstrap/config_dirs/nvim/lua/user/plugins/leap.lua
@@ -1,8 +1,6 @@
--- Leap is a general-purpose motion plugin for Neovim, building and improving primarily on vim-sneak,
--- with the ultimate goal of establishing a new standard interface for moving around in the visible
--- area in Vim-like modal editors. It allows you to reach any target in a very fast, uniform way,
--- and minimizes the required focus level while executing a jump.
-
+-- General-purpose motion plugin. It allows you to reach any target in a very
+-- fast, uniform way, and minimizes the required focus level while executing a jump.
+-- https://github.com/ggandor/leap.nvim
 return {
 	"ggandor/leap.nvim",
 	-- keys = {
@@ -10,7 +8,7 @@ return {
 	--   { "S", mode = { "n", "x", "o" }, desc = "Leap backward to" },
 	--   { "gs", mode = { "n", "x", "o" }, desc = "Leap from windows" },
 	-- },
-	config = function(_, opts)
+	config = function(_, _)
 		local leap = require("leap")
 		-- for k, v in pairs(opts) do
 		--   leap.opts[k] = v
