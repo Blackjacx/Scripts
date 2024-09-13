37c03761d78934bd9d1090ddcfbb773ddd5515bd
Stefan Herold <794372+Blackjacx@users.noreply.github.com>
2024-09-13 10:33:10 +0200

    Add todo file containing interesting plugins
    Additionally this file contains plugins that are not configured
    correctly. The recommendation by Lazy is to use opts, keys, cmd, etc. instead
    of configure.

---
+++ bootstrap/config_dirs/nvim/todo.lua
@@ -0,0 +1,85 @@
+-- TODO: Check the following plugins as they may not be configured correctly (missing "opts = {}")
+
+-- ./bootstrap/config_dirs/nvim/lua/user/plugins/nvim-cmp.lua
+-- ./bootstrap/config_dirs/nvim/lua/user/plugins/todo-comments.lua
+-- ./bootstrap/config_dirs/nvim/lua/user/plugins/nvim-tree.lua
+-- ./bootstrap/config_dirs/nvim/lua/user/plugins/vim-fugitive.lua
+-- ./bootstrap/config_dirs/nvim/lua/user/plugins/lsp/mason.lua
+-- ./bootstrap/config_dirs/nvim/lua/user/plugins/lualine.lua
+-- ./bootstrap/config_dirs/nvim/lua/user/plugins/colorscheme.lua
+-- ./bootstrap/config_dirs/nvim/lua/user/plugins/nvim-autopairs.lua
+-- ./bootstrap/config_dirs/nvim/lua/user/plugins/comment.lua
+-- ./bootstrap/config_dirs/nvim/lua/user/plugins/conform.lua
+-- ./bootstrap/config_dirs/nvim/lua/user/plugins/nvim fr-web-devicons.lua
+-- ./bootstrap/config_dirs/nvim/lua/user/plugins/neogit.lua
+-- ./bootstrap/config_dirs/nvim/lua/user/plugins/nvim-treesitter-text-objects.lua
+-- ./bootstrap/config_dirs/nvim/lua/user/plugins/nvim-surround.lua
+-- ./bootstrap/config_dirs/nvim/lua/user/plugins/nerdy.lua
+-- ./bootstrap/config_dirs/nvim/lua/user/plugins/lazygit.lua
+
+-- TODO: Check out the following interesting plugins:
+
+-- ++++++++++++++++++++++ Misc +++++++++++++++++++++++++++++++++++++++++++++++++
+-- https://github.com/mpas/marp-nvim -- A neovim plugin for Marp - Markdown Presentation Ecosystem
+-- https://github.com/m4xshen/hardtime.nvim -- Helping you establish good command workflow and habit
+-- https://github.com/akinsho/toggleterm.nvim -- A neovim lua plugin to help easily manage multiple terminal windows
+-- https://github.com/ThePrimeagen/harpoon -- Getting you where you want with the fewest keystrokes
+-- https://github.com/nvim-pack/nvim-spectre -- A search panel for neovim
+-- https://github.com/ms-jpq/chadtree -- File manager for Neovim. Better than NERDTree.
+-- https://github.com/RRethy/vim-illuminate -- (Neo)Vim plugin for automatically highlighting other uses of the word under the cursor using either LSP, Tree-sitter, or regex matching.
+-- https://github.com/danymat/neogen - A better annotation generator. Supports multiple languages and annotation conventions.
+-- https://github.com/rgroli/other.nvim -- Open alternative files for the current buffer
+-- https://github.com/axieax/urlview.nvim -- Neovim plugin for viewing all the URLs in a buffer
+-- https://github.com/crusj/bookmarks.nvim -- Remember file locations and sort by time and frequency and support telescope.
+-- https://github.com/kevinhwang91/nvim-ufo -- make Neovim's fold look modern and keep high performance.
+-- https://github.com/jbyuki/instant.nvim -- collaborative editing in Neovim using built-in capabilities
+-- https://github.com/zbirenbaum/neodim -- Neovim plugin for dimming the highlights of unused functions, variables, parameters, and more
+-- https://github.com/ecthelionvi/NeoComposer.nvim -- Neovim plugin that simplifies macros, enhancing productivity with harmony.
+-- https://github.com/cshuaimin/ssr.nvim -- Treesitter based structural search and replace plugin for Neovim.
+-- https://github.com/petertriho/nvim-scrollbar -- Extensible Neovim Scrollbar
+-- https://github.com/HiPhish/rainbow-delimiters.nvim -- Rainbow delimiters for Neovim with Tree-sitter
+-- https://github.com/pocco81/true-zen.nvim -- 🦝 Clean and elegant distraction-free writing for NeoVim
+-- https://github.com/folke/zen-mode.nvim - 🧘 Distraction-free coding for Neovim
+-- https://github.com/debugloop/telescope-undo.nvim -- A telescope extension to view and search your undo tree
+
+-- ++++++++++++++++++++++ Running Code +++++++++++++++++++++++++++++++++++++++++
+-- https://github.com/michaelb/sniprun -- A neovim plugin to run lines/blocs of code
+
+-- ++++++++++++++++++++++ Preconfigured Distributions ++++++++++++++++++++++++++
+-- https://github.com/NvChad/NvChad -- An attempt to make Neovim cli as functional as an IDE while being very beautiful and less bloated.
+-- https://github.com/LunarVim/LunarVim -- This project aims to help one transition away from VSCode, and into a superior text editing experience.
+
+-- ++++++++++++++++++++++ Alternative Statuslines+++++++++++++++++++++++++++++++
+-- https://github.com/rebelot/heirline.nvim
+
+-- ++++++++++++++++++++++ Alternative Bufferlines+++++++++++++++++++++++++++++++
+-- https://github.com/romgrk/barbar.nvim
+
+-- ++++++++++++++++++++++ Markdown Notetaking+++++++++++++++++++++++++++++++++++
+-- https://github.com/epwalsh/obsidian.nvim -- Obsidian 🤝 Neovim
+-- https://github.com/nvim-neorg/neorg -- Modernity meets insane extensibility. The future of organizing your life in Neovim.
+-- https://github.com/OXY2DEV/markview.nvim -- An experimental markdown previewer for Neovim
+-- https://github.com/iamcco/markdown-preview.nvim -- markdown preview plugin for (neo)vim
+
+-- ++++++++++++++++++++++ TODO Management ++++++++++++++++++++++++++++++++++++++
+-- https://github.com/nvim-orgmode/orgmode -- Orgmode clone written in Lua
+-- https://github.com/nvim-telekasten/telekasten.nvim - working with a markdown zettelkasten / wiki and mixing it with a journal, based on telescope.nvim
+
+-- ++++++++++++++++++++++ Debugging ++++++++++++++++++++++++++++++++++++++++++++
+-- https://github.com/mfussenegger/nvim-dap -- Debug Adapter Protocol client implementation for Neovim
+-- https://github.com/rcarriga/nvim-dap-ui -- A UI for nvim-dap:with
+-- https://github.com/Weissle/persistent-breakpoints.nvim -- Neovim plugin for persistent breakpoints
+-- https://github.com/kevinhwang91/nvim-bqf -- Better quickfix window
+
+-- ++++++++++++++++++++++ Testing ++++++++++++++++++++++++++++++++++++++++++++++
+-- https://github.com/nvim-neotest/neotest -- An extensible framework for interacting with tests within Neovim
+
+-- ++++++++++++++++++++++ Git ++++++++++++++++++++++++++++++++++++++++++++++++++
+-- https://github.com/pwntester/octo.nvim -- Edit and review GitHub issues and pull requests from the comfort of your favorite editor
+-- https://github.com/ldelossa/gh.nvim -- A fully featured GitHub integration for performing code reviews in Neovim.
+-- https://github.com/topaxi/gh-actions.nvim -- See status of workflows and dispatch runs directly in neovim
+-- https://github.com/rawnly/gist.nvim -- Neovim plugin that allows you to create a Github Gist from the current file
+-- https://github.com/sindrets/diffview.nvim -- Enhanced diff in nvim.
+
+-- ++++++++++++++++++++++ Motion +++++++++++++++++++++++++++++++++++++++++++++++
+-- https://github.com/folke/flash.nvim -- Navigate your code with search labels, enhanced character motions and Treesitter integration
