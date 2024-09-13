3e701e8951ce7ea5cb13efe5b9e0ab15bc2aa218
Stefan Herold <794372+Blackjacx@users.noreply.github.com>
2024-09-13 10:46:14 +0200

    Use Telescope Keymaps to lazy load plugin

--- bootstrap/config_dirs/nvim/lua/user/plugins/telescope.lua
+++ bootstrap/config_dirs/nvim/lua/user/plugins/telescope.lua
@@ -7,6 +7,25 @@ return {
 		"nvim-tree/nvim-web-devicons",
 		"folke/todo-comments.nvim",
 	},
+	keys = {
+		-- Files
+		{ "<leader>ff", ":Telescope find_files<CR>", desc = "Fuzzy find files in cwd" }, -- find files within current working directory, respects .gitignore
+		{ "<leader>fs", ":Telescope live_grep<CR>", desc = "Find string in cwd" },
+		{ "<leader>fc", ":Telescope grep_string<CR>", desc = "Find string under cursor in cwd" }, -- find string under cursor in current working directory
+		{ "<leader>fr", ":Telescope oldfiles<CR>", desc = "Fuzzy find recent files" },
+		{ "<leader>fb", ":Telescope buffers<CR>", desc = "List open buffers" },
+		{ "<leader>fj", ":Telescope jumplist<CR>", desc = "Open jumplist" },
+		{ "<leader>fh", ":Telescope help_tags<CR>", desc = "List Help Tags" },
+
+		-- ToDo Comment
+		{ "<leader>ft", ":TodoTelescope<CR>", desc = "Find todos" }, -- find todos in cwd
+
+		-- Git
+		{ "<leader>gC", ":Telescope git_commits<CR>", desc = "All Commits" }, -- use <cr> to checkout ["gc" for git commits]
+		{ "<leader>gh", ":Telescope git_bcommits<CR>", desc = "File History" }, -- list git commits for current file/buffer (use <cr> to checkout) ["gfc" for git file commits]
+		{ "<leader>gb", ":Telescope git_branches<CR>", desc = "Git Branches" }, -- list git branches (use <cr> to checkout) ["gb" for git branch]
+		{ "<leader>gs", ":Telescope git_status<CR>", desc = "Git Status" },
+	},
 	config = function()
 		local telescope = require("telescope")
 		local actions = require("telescope.actions")
@@ -31,26 +50,5 @@ return {
 
 		telescope.load_extension("fzf")
 		telescope.load_extension("nerdy")
-
-		-- Keymaps
-
-		local keymap = vim.keymap
-
-		-- Files
-		keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", { desc = "Fuzzy find files in cwd" }) -- find files within current working directory, respects .gitignore
-		keymap.set("n", "<leader>fs", ":Telescope live_grep<CR>", { desc = "Find string in cwd" })
-		keymap.set("n", "<leader>fc", ":Telescope grep_string<CR>", { desc = "Find string under cursor in cwd" }) -- find string under cursor in current working directory
-		keymap.set("n", "<leader>fr", ":Telescope oldfiles<CR>", { desc = "Fuzzy find recent files" })
-		keymap.set("n", "<leader>fb", ":Telescope buffers<CR>", { desc = "List open buffers" })
-		keymap.set("n", "<leader>fh", ":Telescope help_tags<CR>", { desc = "List Help Tags" })
-
-		-- ToDo Comment
-		keymap.set("n", "<leader>ft", ":TodoTelescope<CR>", { desc = "Find todos" }) -- find todos in cwd
-
-		-- Git
-		keymap.set("n", "<leader>gC", ":Telescope git_commits<CR>", { desc = "All Commits" }) -- use <cr> to checkout ["gc" for git commits]
-		keymap.set("n", "<leader>gh", ":Telescope git_bcommits<CR>", { desc = "File History" }) -- list git commits for current file/buffer (use <cr> to checkout) ["gfc" for git file commits]
-		keymap.set("n", "<leader>gb", ":Telescope git_branches<CR>", { desc = "Git Branches" }) -- list git branches (use <cr> to checkout) ["gb" for git branch]
-		keymap.set("n", "<leader>gs", ":Telescope git_status<CR>", { desc = "Git Status" })
 	end,
 }
