151d1d062d588ca6af13a1fa8a22526b7caf8cc9
Stefan Herold <794372+Blackjacx@users.noreply.github.com>
2025-08-05 11:23:28 +0200

    Remove battery status not needed in shell prompt
    This is now displayed in the TMUX status bar

--- bootstrap/dotfiles/.p10k.zsh
+++ bootstrap/dotfiles/.p10k.zsh
@@ -49,7 +49,7 @@
     status                  # exit code of the last command
     command_execution_time  # duration of the last command
     background_jobs         # presence of background jobs
-    #direnv                  # direnv status (https://direnv.net/)
+    # direnv                  # direnv status (https://direnv.net/)
     asdf                    # asdf version manager (https://github.com/asdf-vm/asdf)
     virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
     anaconda                # conda environment (https://conda.io/)
@@ -77,7 +77,7 @@
     # scalaenv                # scala version from scalaenv (https://github.com/scalaenv/scalaenv)
     # haskell_stack           # haskell version from stack (https://haskellstack.org/)
     # kubecontext             # current kubernetes context (https://kubernetes.io/)
-    terraform               # terraform workspace (https://www.terraform.io)
+    # terraform               # terraform workspace (https://www.terraform.io)
     # terraform_version     # terraform version (https://www.terraform.io)
     # aws                     # aws profile (https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-profiles.html)
     # aws_eb_env              # aws elastic beanstalk environment (https://aws.amazon.com/elasticbeanstalk/)
@@ -105,7 +105,7 @@
     timewarrior             # timewarrior tracking status (https://timewarrior.net/)
     # taskwarrior             # taskwarrior task count (https://taskwarrior.org/)
     # cpu_arch              # CPU architecture
-    battery               # internal battery
+    # battery               # internal battery
     time                    # current time
     # =========================[ Line #2 ]=========================
     newline
