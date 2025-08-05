2aa8d8ae0ab3ecdea7abcb32aba19682f6af0d98
Stefan Herold <794372+Blackjacx@users.noreply.github.com>
2025-08-05 11:58:42 +0200

    Extend time warrior backup by tw-charts
    tw-charts is a (not yet) public tool to print charts from your time
    warrior tags (`tw export ioki | tw-chart`):
    
    ```
    Time Spent by Tag Combination:
    
     42.3% | ████████████████ job, misc
     29.3% | ███████████ coding, job
     15.5% | ██████ job, review
     10.1% | ████ job
      2.2% |  job, training
      0.2% |  job, a11y-audit
      0.0% |  job, meeting
    ```

--- cron/backup-timewarrior-daily.sh
+++ cron/backup-timewarrior-daily.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env zsh
 
-# Backs up my timewarrior daily working time info from YESTERDAY so that I 
-# have a chance to find  out what went wrong with balance report. 
+# Backs up my timewarrior daily working time info from YESTERDAY so that I
+# have a chance to find  out what went wrong with balance report.
 #
-# It should run one minute after midnight so it does not capture the time when 
+# It should run one minute after midnight so it does not capture the time when
 # working past midnight.
-# 
+#
 # The generated timestamp always prints the time from yesterday.
 
-echo "$(timew balance ioki 2023-11-13 - today && timew summary :yesterday :ids)" > "~/docs/_backups/timewarrior/working-time/$(date -v-1d +%F-%A)"
+fname="${HOME}/docs/_backups/timewarrior/working-time/$(date -v-1d +%F-%A)"
+echo "$(timew balance ioki 2023-11-13 - today && timew summary :yesterday :ids && timew export ioki | tw-charts)" >"$fname"
