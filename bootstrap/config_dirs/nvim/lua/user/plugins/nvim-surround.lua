0329067687b9a9e501cc193637e2b8f679582f41
Stefan Herold <794372+Blackjacx@users.noreply.github.com>
2025-08-12 17:31:50 +0200

    Use very lazy setup event for nvim-lint

--- bootstrap/config_dirs/nvim/lua/user/plugins/nvim-surround.lua
+++ bootstrap/config_dirs/nvim/lua/user/plugins/nvim-surround.lua
@@ -2,7 +2,8 @@
 return {
 	"kylechui/nvim-surround",
 	version = "*", -- Use for stability; omit to use `main` branch for the latest features
-	event = { "BufReadPre", "BufNewFile" },
+	-- event = { "BufReadPre", "BufNewFile" },
+	event = { "VeryLazy" },
 	config = function()
 		require("nvim-surround").setup({
 			-- Configuration here, or leave empty to use defaults
